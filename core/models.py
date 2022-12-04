d9b790af3d44a6f165549d18e0181acb9ee1de83
kvarel <kvarel2509@gmail.com>
2022-12-04 23:10:24 +0300

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.conf import settings
+from typing import List
+
+
+class TestGroup(models.Model):
+	"""Группа тестов"""
+
+	title = models.CharField('Название группы', max_length=50)
+	description = models.TextField('Описание группы', blank=True)
+
+	class Meta:
+		verbose_name = 'Группа тестов'
+		verbose_name_plural = 'Группы тестов'
+
+	def __str__(self):
+		return self.title
+
+
+class Test(models.Model):
+	"""Тест"""
+
+	title = models.CharField('Название теста', max_length=50)
+	description = models.TextField('Описание теста', blank=True)
+	groups = models.ManyToManyField(TestGroup, verbose_name='Принадлежность к группам', blank=True, null=True)
+
+	class Meta:
+		verbose_name = 'Тест'
+		verbose_name_plural = 'Тесты'
+
+	def __str__(self):
+		return self.title
+
+
+class Question(models.Model):
+	"""Вопрос теста"""
+
+	title = models.TextField('Текст вопроса')
+	test = models.ForeignKey(Test, on_delete=models.CASCADE, verbose_name='Принадлежность к тесту')
+	position = models.IntegerField('Позиция вопроса в тесте')
+	visibility = models.BooleanField('Видимость вопроса', default=True)
+
+	class Meta:
+		verbose_name = 'Вопрос'
+		verbose_name_plural = 'Вопросы'
+
+	def __str__(self):
+		return self.title
+
+	def check_answer(self, answer: List[int]):
+		right_answer = self.answer_set.filter(is_right=True).values_list('id', flat=True)
+		return sorted(right_answer) == sorted(answer)
+
+
+class Answer(models.Model):
+	"""Ответ на вопрос"""
+
+	title = models.TextField('Текст ответа')
+	is_right = models.BooleanField('Правильный ответ', default=False)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='Принадлежность к вопросу')
+
+	class Meta:
+		verbose_name = 'Ответ'
+		verbose_name_plural = 'Ответы'
+
+	def __str__(self):
+		return self.title
+
+
+class Testing(models.Model):
+	"""История прохождения тестов"""
+
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+	test = models.ForeignKey(Test, on_delete=models.CASCADE, verbose_name='Тест')
+	date = models.DateTimeField('Дата прохождения', auto_now_add=True)
+	correct_answers = models.IntegerField('Количество правильных ответов')
+	wrong_answers = models.IntegerField('Количество неправильных ответов')
+	is_completed = models.BooleanField('Корректно завершен')
